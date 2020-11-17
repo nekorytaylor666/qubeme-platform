55c337040d74c66131ec19ea544be0a7ed3e83d9
Tokhtar <nekorytaylor2@gmail.com>
2020-11-17 19:41:45 +0600

    im in despair

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,6 @@ import fetch from 'isomorphic-unfetch';
 import cookies from 'next-cookies';
 import UserProvider from '../context/UserContext';
 import App from 'next/app';
-import { validateToken } from '../utils/auth/validateToken';
 
 function MyApp({ user, Component, pageProps }) {
 	return (
@@ -14,6 +13,29 @@ function MyApp({ user, Component, pageProps }) {
 }
 
 MyApp.getInitialProps = async (appContext) => {
+	const validateToken = async (token) => {
+		const dev = process.env.NODE_ENV === 'development';
+		const public_url = process.env.VERCEL_URL;
+		//!TODO change to env var
+		const server = dev ? 'http://localhost:3000' : public_url;
+
+		// If token exists run Firebase validation on server side before rendering.
+		if (token) {
+			try {
+				const headers = {
+					'Context-Type': 'application/json',
+					Authorization: JSON.stringify({ token: token }),
+				};
+				console.log('variables:', dev, server, public_url);
+				const url = `${server}/api/validate/`;
+				console.log('api url:', url);
+				const result = await fetch(url, { headers, mode: 'cors' }).then((res) => res.json());
+				return result;
+			} catch (e) {
+				throw e;
+			}
+		}
+	};
 	const { ctx } = appContext;
 	// Calls `getInitialProps` and fills `appProps.pageProps`
 	let error;
@@ -22,6 +44,7 @@ MyApp.getInitialProps = async (appContext) => {
 	const { firebaseToken } = cookies(ctx);
 	try {
 		// If token exists run Firebase validation on server side before rendering.
+
 		const result = await validateToken(firebaseToken);
 		console.log(result);
 		const props = { ...result, ...appProps };
